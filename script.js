2857b563101bb7fabca5f3a5e0b66fb6e4c7003c
Amit Kumar <amitk15042003@gmail.com>
2024-09-13 17:38:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const itemButton = document.querySelector('#itemButt');
+const itemList = document.querySelector('.items-list');
+
+
+   itemButton.addEventListener('click',function(){
+   itemList.classList.toggle('active');
+			});
+	
